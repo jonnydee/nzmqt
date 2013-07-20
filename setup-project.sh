1dd56858c7c8ac5361037d51a356f1f1e5e4f8d8
Jonny Dee <jonny.dee@gmx.net>
2013-07-20 12:34:05 +0200

    cppzmq: Removed patch as pull request https://github.com/zeromq/cppzmq/issues/27 has been accepted.

--- setup-project.sh
+++ setup-project.sh
@@ -7,6 +7,4 @@ mkdir -p externals/include
 
 # Copy cppzmq related files.
 cp externals-src/cppzmq/zmq.hpp externals/include
-# Patch cppzmq files where necessary.
-git apply --directory externals/include externals-src/cppzmq.patch
 
